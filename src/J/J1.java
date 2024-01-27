6cc45e50fb3c62a0e4f84e0304af4327e3a0bb2d
Aizirek cb2 <aizire4ka98@gmail.com>
2024-01-27 15:49:13 +0600

    Revert

---
+++ src/J/J1.java
@@ -0,0 +1,7 @@
+package J;
+
+public class J1 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
--- src/L1.java
+++
@@ -1,5 +0,0 @@
-public class L1 {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-}
